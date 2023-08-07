8ca79f45cccecd11670b02aadc9157269d2bb8a8
Shoneel <shoneelkumar@gmail.com>
2023-08-07 22:15:25 +1200

    Deploy Site Changes

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -7,7 +7,7 @@ export const Data = [
     id: 1,
     image: Image1,
     title: "Project Modern Website",
-    link: "",
+    link: "https://shoneel.github.io/modern-website-project/",
     description:
       "A modern ReactJS website with dynamic and interactive web application built using the React JavaScript library.  ",
   },
