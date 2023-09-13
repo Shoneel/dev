4dc42811cdace9f1d8eeb4eadd876f7ded37bd9d
Shoneel <shoneelkumar@gmail.com>
2023-09-13 17:19:31 +1200

    Update Project List

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -1,14 +1,43 @@
-import Image1 from "../../assets/project-modern-website.png";
-
+import Image1 from "../../assets/portfolio-projects/Project-Fitness-App.png";
+import Image2 from "../../assets/portfolio-projects/Project-Emenu-App.png";
+import Image3 from "../../assets/portfolio-projects/Project-Ecommerce-App.png";
+import Image4 from "../../assets/portfolio-projects/project-modern-website.png";
 
 // ========== PORTFOLIO DATA ==========
 export const Data = [
   {
     id: 1,
     image: Image1,
-    title: "Project Modern Website",
+    title: "Fitness Exercise App",
+    link: "https://fitness-exercise-app-prod.netlify.app/",
+    description:
+      "Created a modern fitness app using two RapidAPIs, allowing users to explore 1000+ exercises, choose categories/muscle groups, view examples, videos, and similar exercises.",
+  },
+
+  {
+    id: 2,
+    image: Image2,
+    title: "eMenu App",
+    link: "https://shoneel.github.io/eMenu-App/",
+    description:
+      "Crafted a fully responsive modern E-menu/food website from scratch using HTML, CSS, and JavaScript, delivering an engaging user experience.",
+  },
+
+  {
+    id: 3,
+    image: Image3,
+    title: "eCommerce App",
+    link: "https://e-commerce-app-prod.netlify.app/",
+    description:
+      "Mastered the art of building a top-tier, modern eCommerce site with Next.js and integrated the Sanity CMS and sleek design with real payments via Stripe.",
+  },
+
+  {
+    id: 4,
+    image: Image4,
+    title: "Modern Website",
     link: "https://shoneel.github.io/modern-website-project/",
     description:
-      "A modern ReactJS website with dynamic and interactive web application built using the React JavaScript library.  ",
+      "Expertly converted Figma designs into responsive ReactJS websites with reusable components, showcasing advanced CSS skills and modern web development.",
   },
 ];
